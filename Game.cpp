8a591620ca0d69ad7c56897a636a00da3e59e303
GeorgiD31 <101389914+GeorgiD31@users.noreply.github.com>
2023-03-09 19:03:58 +0200

    clearer player movement

--- Game.cpp
+++ Game.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <Windows.h>
+#include <conio.h>
 #include <fstream>
 
 using namespace std;
 
-const int HEIGHT = 5, WIDTH = 20;
+const int HEIGHT = 30, WIDTH = 55;
 int x = 0, y = 0; //Primary player coordinates...global :-O
 
 void GrassColor() {
@@ -27,18 +28,25 @@ void RedColor() {
         (GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
 }
 
+void drawPlayer(int x, int y) {
+    // gotoxy(x, y, '@');
+    printf("\033[%d;%dH@", (y), (x));
+}
+
+void clearPlayer(int x, int y) {
+    printf("\033[%d;%dH*", (y), (x));
+}
+
 // Read from the text file
 char myMap[HEIGHT][WIDTH];
 
 void PrintMap() {
+    system("cls");
     cout << "(" << x << ',' << y << ")" << endl;
     GrassColor();
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            if (myMap[i][j] == '\n'){
-                i++;
-            }
-            else if (myMap[i][j] == '.') {
+            if (myMap[i][j] == '.') {
                 GrassColor();
                 cout << myMap[i][j];
             }
@@ -74,43 +82,80 @@ int main() {
     MyReadFile.close();
     PrintMap();
 
+
+    while (1) {
+        drawPlayer(x, y);
+
+        char input = getch();
+
+        switch (input) {
+            case 'w':
+                clearPlayer(x, y);
+                y--;
+                break;
+            case 'a':
+                clearPlayer(x, y);
+                x--;
+                break;
+            case 's':
+                clearPlayer(x, y);
+                y++;
+                break;
+            case 'd':
+                clearPlayer(x, y);
+                x++;
+                break;
+            case 'q':
+                return 0;
+        }
+    }
+
+/*
     while (true) {
-        cin >> input;
+        input = getch(); // get user input
 
         if (input == 's' && y < HEIGHT - 1) {
+    
             myMap[y][x] = temp_tile;
             y++;
             temp_tile = myMap[y][x];
             myMap[y][x] = '@';
+             system("cls");
             PrintMap();
+            
         }
-        else if (input == 'e' && x < WIDTH - 1) {
+        else if (input == 'd' && x < WIDTH - 1) {
+            
             myMap[y][x] = temp_tile;
             x++;
             temp_tile = myMap[y][x];
             myMap[y][x] = '@';
+             system("cls");
             PrintMap();
         }
-        else if (input == 'n' && y > 0) {
+        else if (input == 'w' && y > 0) {
+         
             myMap[y][x] = temp_tile;
             y--;
             temp_tile = myMap[y][x];
             myMap[y][x] = '@';
+             system("cls");
             PrintMap();
         }
-        else if (input == 'w' && x > 0) {
+        else if (input == 'a' && x > 0) {
+           
             myMap[y][x] = temp_tile;
             x--;
             temp_tile = myMap[y][x];
             myMap[y][x] = '@';
+             system("cls");
             PrintMap();
         }
         else if(input == 'q'){
             break;
         };
-        
     }
-
     system("PAUSE");
+    */
     return 0;
 }
